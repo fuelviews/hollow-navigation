f365f7b87b054f9bdf3d2d7f76d3be91f51eeceb
Daniel <daniel@fuelviews.com>
2025-11-24 23:20:28 -0500

    feat: Update navigation button and link styles to include smoother border color transitions and improved hover effects.

--- resources/views/components/desktop/desktop-dropdown-button.blade.php
+++ resources/views/components/desktop/desktop-dropdown-button.blade.php
@@ -1,7 +1,7 @@
 <button
-    class="flex items-center text-sm leading-5 font-medium hover:text-gray-400 focus:outline-none transition ease-in-out duration-150 uppercase">
+    class="flex items-center text-sm leading-5 font-medium hover:text-gray-400 focus:outline-none transition-[border-color] duration-300 ease-in-out uppercase">
     <div
-        class="{{ Navigation::isDropdownRouteActive($links) ? 'border-prime-400' : 'border-transparent' }} border-b-2 hover:border-gray-300 hover:border-b-2 py-2">
+        class="{{ Navigation::isDropdownRouteActive($links) ? 'border-prime-400' : 'border-transparent' }} border-b-2 hover:border-gray-300 hover:border-b-2 py-2 transition-[border-color] duration-300">
         {{ $name }}
     </div>
     <x-navigation::dropdown-icon/>
